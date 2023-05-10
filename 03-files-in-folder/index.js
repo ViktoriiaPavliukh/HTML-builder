c6b8a0536ae834170644455c0077c84af3216f0f
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-05-10 16:26:58 +0100

    fix: remove extention near file name

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,10 +6,11 @@ async function readSecretFolder() {
     const files = await fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
     files.forEach(async(file) => {
       if(file.isFile()) {
-        const extName = path.extname(file.name);
+        const fileName = path.parse(file.name).name;
+        const extName = path.extname(file.name).slice(1);
         const stats = await fs.stat(path.join(__dirname, 'secret-folder', file.name));
         const fileSize = stats.size;
-        console.log(`${file.name}-${extName}-${fileSize}`);
+        console.log(`${fileName}-${extName}-${fileSize}`);
       }
     });
   } catch (err) {
