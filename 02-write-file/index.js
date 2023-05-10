178b5d613cd4757a34815a4338a71d0090ed93f0
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-05-10 16:04:20 +0100

    fix: add message for exit by Ctrl+C

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,37 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
 
 const filePath = path.join(__dirname, 'text.txt');
-const writeText = fs.createWriteStream(filePath, { flags: 'a'});
+const writeText = fs.createWriteStream(filePath);
 
-console.log('Please enter text');
-
-const interface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: '',
-});
-
-interface.on('line', (input) => {
-  if(input.toLowerCase() === 'exit') {
-    console.log('Bye');
-    process.exit(0);
-  } else {
-    writeText.write(`${input}\n`)
-  }
-})
-
-process.on('SIGINT', () => {
-  process.stdout.write('See you!\n');
-  writeText.end(() => {
-    process.exit(0);
-  });
-});
-
-process.on('SIGTERM', () => {
-  process.stdout.write('See you!\n');
-  writeText.end(() => {
-    process.exit(0);
-  });
-});
+process.stdout.write('Enter your text:');
+process.stdin.on('data', function (data) {
+    if (data.toString().trim() === 'exit') {
+        process.stdout.write('Bye')
+        process.exit();
+    }
+      writeText.write(data);
+ });
+ 
+ process.on('SIGINT', function(){
+    process.stdout.write('See you')
+    process.exit();
+ });
